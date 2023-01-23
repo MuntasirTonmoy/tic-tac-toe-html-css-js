6d48af75ffee153af5a8935e89895e8ccc522f2e
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-23 22:38:05 +0600

    vaiable created

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+// selectors
 const boxes = document.querySelectorAll(".box");
+
+//variables
+const O_TEXT = "O";
+const X_TEXT = "X";
 
 boxes.forEach((box, index) => {
   box.addEventListener("click", boxClicked);
