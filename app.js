38ca02ebec7fe719d9cfe8e82b7521c8b60f7b57
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-23 23:50:07 +0600

    checking if the first row matches

--- app.js
+++ app.js
@@ -9,16 +9,20 @@ const boxFilled = [null, null, null, null, null, null, null, null, null];
 
 boxes.forEach(function (box, index) {
   box.addEventListener("click", boxClicked);
-  console.log();
 });
 
 function boxClicked(event) {
   const boxIndex = event.target.dataset.boxIndex;
-  console.log(boxIndex);
 
   if (boxFilled[boxIndex] === null) {
     boxFilled[boxIndex] = currentPlayer;
     event.target.innerText = currentPlayer;
+
+    const firstRow =
+      boxFilled[0] === currentPlayer &&
+      boxFilled[1] === currentPlayer &&
+      boxFilled[2] === currentPlayer;
+
     currentPlayer = currentPlayer === O_TEXT ? X_TEXT : O_TEXT;
   }
 }
