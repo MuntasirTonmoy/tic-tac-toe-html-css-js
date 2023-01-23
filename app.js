1b5dab972e63f5a43efc395099b758bc9d9ef493
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2023-01-23 22:49:15 +0600

    data added on markup

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const boxes = document.querySelectorAll(".box");
 //variables
 const O_TEXT = "O";
 const X_TEXT = "X";
+const currentPlayer = O_TEXT;
+const boxFilled = [null, null, null, null, null, null, null, null, null];
 
 boxes.forEach((box, index) => {
   box.addEventListener("click", boxClicked);
 });
 
-function boxClicked(event) {}
+function boxClicked(event) {
+  const boxNo = event.target.
+}
